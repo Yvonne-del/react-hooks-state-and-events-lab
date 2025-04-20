e8d55cc9db88a5e8cd44de2b0c4e50a05f417d32
Yvonne-del <ys359375@gmail.com>
2025-04-20 18:04:03 +0300

    ..

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  //cart variable to hold value of className
+  // value false says the className is empty "" meaning item is not added to cart yet
+  const [cart, setCart]=useState(false)
+
+  // if className is is true, classname value is "in-cart"
+  const cartFunction = cart ? "in-cart" : ""
+  
+  // function to change the value of className  to true or false based on the current value
+  //which at first is false, but when button is clicked, it changes to true 
+  function handleClick(){
+    setCart(() => !cart)
+  }
+
   return (
-    <li className="">
+    <li className={cartFunction}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick} >{cart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  //note items prop is the data called in App
+
+  //create a varibale to hold value of the options and default value of All
+  const [selectedCategory, setSelectedCategory] = useState("All")
+  //change value of selectedCategory based on chosen option value
+  function handleChange(e){
+    setSelectedCategory(e.target.value)
+  }
+  //filter the items based on value of category
+  const filterCategory = items.filter((item)=>{
+    if(selectedCategory === "All"){
+      return true; //to return all as true...all items are listed
+    }else{
+      return item.category === selectedCategory;
+    }
+  })
+
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +31,8 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {/* list items based on the liost of filtered items */}
+        {filterCategory.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
